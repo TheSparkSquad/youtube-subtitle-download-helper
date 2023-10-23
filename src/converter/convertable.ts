287192336a7b32f27bc2c14fe9c5481b3802c8a9
lonespartan12 <lonespartan12@hotmail.com>
2023-10-23 14:15:07 -0600

    testing the download location
    trying to change the download location to the public documents folder to see if I can see if I can change it.

--- src/converter/convertable.ts
+++ src/converter/convertable.ts
@@ -1,6 +1,6 @@
 import { Alines } from "../type/aline";
 
 export interface Convertable {
-  convert(xmlResponse: string, fileName: string): void;
+  convert(xmlResponse: string, fileName: string): void; //CMudd this looks promising for where the file directory is set
   format(xmlResponse: string): Alines;
 }
